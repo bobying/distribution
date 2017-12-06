95d70bd1595b206e6f5770f9756fb82c682ee5f0
bob.ying <124599792@qq.com>
2017-12-06 14:00:28 +0800

    Initial application generated by JHipster-4.11.1

---
+++ src/main/java/com/cloud/distribution/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package com.cloud.distribution.config.audit;
---
+++ src/main/java/com/cloud/distribution/security/SpringSecurityAuditorAware.java
@@ -0,0 +1,18 @@
+package com.cloud.distribution.security;
+
+import com.cloud.distribution.config.Constants;
+
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.stereotype.Component;
+
+/**
+ * Implementation of AuditorAware based on Spring Security.
+ */
+@Component
+public class SpringSecurityAuditorAware implements AuditorAware<String> {
+
+    @Override
+    public String getCurrentAuditor() {
+        return SecurityUtils.getCurrentUserLogin().orElse(Constants.SYSTEM_ACCOUNT);
+    }
+}
